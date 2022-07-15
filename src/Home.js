528ed7b2aec529b3206ea5546ae5f82a10b1708a
George Guardia <georgeguardia01@gmail.com>
2022-07-15 00:57:23 -0400

    Navbar and css format

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+const Home = () => {
+
+    const handleClick = () =>{
+        console.log('hello');
+    }
+
+    return ( 
+        <div className="Home">
+            <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+        </div>
+     );
+}
+ 
+export default Home;
+<div className="Home">
+
+</div>
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Game Blog Spot</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create" style={{
+                }}>New Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
