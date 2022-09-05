a4c9b0dbed69a6566a9c36a451a86bbfd9e93c4b
George Guardia <georgeguardia01@gmail.com>
2022-09-05 18:32:34 -0400

    useFetch and JSON

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Bloglist from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: 'DOOM (2020)', body: 'lorem ipsum...', author: 'Jean Paul', id: 1},
-        {title: 'Elden Ring', body: 'lorem ipsum...', author: 'Brandon Timber', id: 2},
-        {title: 'Escape from Tarkov', body: 'lorem ipsum...', author: 'Nelson Weathers', id: 3}
-    ]);
+    const {data: blogs, isPending, error}  = useFetch('http://localhost:8000/blogs');
 
-    return ( 
+
+    return (
         <div className="Home">
-           <Bloglist blogs = {blogs} title = "All Blogs"/>
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            {blogs && <Bloglist blogs={blogs} title="All Blogs" />}
         </div>
-     );
+    );
 }
- 
+
 export default Home;
